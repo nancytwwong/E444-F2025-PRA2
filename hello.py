723f79cb401971fcae2bac20a409cb2e43c78f60
Nancy Wong <nancytwwong@gmail.com>
2025-09-20 00:50:32 -0400

    Example 3-3

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #Example 2-1. hello.py: A complete Flask application
 #Example 2-2. hello.py: Flask application with a dynamic route
-from flask import Flask
+"""from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,3 +13,15 @@ def user(name):
 
 if __name__ == '__main__':
     app.run()
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
