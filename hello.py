a24208fff90b3002247622df72447927614087a5
Nancy Wong <nancytwwong@gmail.com>
2025-09-20 02:40:08 -0400

    Example 3-5 Debug 3

--- hello.py
+++ hello.py
@@ -16,10 +16,13 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
@@ -27,7 +30,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True)
